efb27fbfe5702363fd0942892b02693ff46b9d7b
Angela Wolff <warriorwomenblog@gmail.com>
2019-08-08 10:21:51 +0100

    reating customise action for toggling blog status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[show edit update destroy toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -54,6 +54,11 @@ class BlogsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
     end
+  end
+
+  def toggle_status
+    @blog.published? ? @blog.draft! : @blog.published!
+    redirect_to blogs_url, notice: 'Post status has been updated'
   end
 
   private
